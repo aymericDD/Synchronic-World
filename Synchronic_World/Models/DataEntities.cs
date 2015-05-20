1c314cfb6def8d08b5f02986c532115a49edda7b
Aymeric <aymeric42680@gmail.com>
2015-05-20 14:56:19 +0200

    Project create

---
+++ Synchronic_World/Models/DataEntities.cs
@@ -0,0 +1,25 @@
+﻿using Synchronic_World.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Synchronic_World
+{
+    public class DataEntities:DbContext
+    {
+        public DbSet<User> UserTable { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasMany<User>(s => s.friends).WithMany(s => s.friends).Map(mc => {
+                mc.ToTable("Friend");
+                mc.MapLeftKey("SenderId");
+                mc.MapRightKey("ReceiverId");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Synchronic_World/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Synchronic_World.Models
+{
+    public class User
+    {
+        public User()
+        {
+            friends = new HashSet<User>();
+        }
+
+        [Key, HiddenInput(DisplayValue = false)]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "User email")]
+        [EmailAddressAttribute]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [Display(Name = "User password")]
+        public string UserPassword { get; set; }
+
+        [Display(Name = "Enrollment date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime JoinDate { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public virtual ICollection<User> friends { get; set; }
+    }
+}
